9e0605f123cbc598b913edc8bd6f7b2d4a4e2f6c
mariaclaraoliveira15-a11y <maria.clara.oliveira15@escola.pr.gov.br>
2026-05-26 09:09:09 -0300

    Add files via upload

---
+++ batalha.js
@@ -0,0 +1,6 @@
+fuction: lutar(){
+    const classe= document.getElementById('classe').ariaValueMax;
+    const arma= document.getElementById('arma');value.trim
+    const log = document.getElementById('log');
+    
+} 
