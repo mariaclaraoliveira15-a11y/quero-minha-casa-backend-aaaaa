d8ff2e386f17b337aa5b49337d4ee559526f65f3
mariaclaraoliveira15-a11y <maria.clara.oliveira15@escola.pr.gov.br>
2026-05-27 11:05:26 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -1,6 +1,21 @@
-fuction: lutar(){
-    const classe= document.getElementById('classe').ariaValueMax;
-    const arma= document.getElementById('arma');value.trim
+
+function lutar(){
+    const classe = document.getElementById('classe').value;
+    const arma = document.getElementById('arma').value.trim() ;
     const log = document.getElementById('log');
     
-} 
+    if(!arma){
+    log.innerHTML= "Você esqueceu de equipar uma arma"
+    }
+    let dado = Math.floor(Math.random() * 20) + 1;
+    let resultado= 'dado ${dado} | <strong>'{classe} </strong> com <strong${arma} </strong>;
+    switch(classe){
+        case "guerreiro";
+        if(arma.tolowerCase()==="espada" && dado >5)
+            resultado+="<span classe= 'sucesso'>SUCESSO: você decptou o monstro com um golpe de espada!</span"
+    }else if (dado>15){
+        resultado+="<spam> classe 'sucesso' > VITORIA: na força bruta, você o esmagou o inimigo com seu "=arma"</span>  
+          } else
+
+}
+    
